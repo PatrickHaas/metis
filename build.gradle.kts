4b66837cdfba9d21a362fb1fa52435990271ce54
Patrick Haas <patrick.haas@gmx.net>
2023-10-12 18:41:15 +0200

    added sonar to ci

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    id("org.sonarqube") version "4.4.1.3373"
 }
 
 group = "io.metis"
@@ -7,6 +8,14 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "PatrickHaas_metis")
+        property("sonar.organization", "patrick-haas")
+        property("sonar.host.url", "https://sonarcloud.io")
+    }
 }
 
 tasks.test {
