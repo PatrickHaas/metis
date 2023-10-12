75d03142860ba2a0509c30acf9d33ddb3c3fd09e
Patrick Haas <patrick.haas@gmx.net>
2023-10-12 22:39:51 +0200

    change email regex pattern to a RFC 5322 Official Standard compatible one

--- metis-employees/metis-employees-domain/src/main/java/io/metis/employees/domain/employee/EmailAddress.java
+++ metis-employees/metis-employees-domain/src/main/java/io/metis/employees/domain/employee/EmailAddress.java
@@ -7,8 +7,7 @@ import java.util.regex.Pattern;
 @ValueObject
 public record EmailAddress(String value) {
 
-    static final Pattern PATTERN = Pattern.compile("^[\\w-.]+@([\\w-]+\\.)+[\\w-]{2,4}$");
-
+    static final Pattern PATTERN = Pattern.compile("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])");
     public EmailAddress {
         if (value == null) {
             throw new IllegalArgumentException("email address must not be null");
