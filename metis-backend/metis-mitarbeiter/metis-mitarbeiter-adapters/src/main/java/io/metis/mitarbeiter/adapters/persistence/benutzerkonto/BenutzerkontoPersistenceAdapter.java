8f489b49f378c7c5514be1a56951ca1c6e7d91ec
Patrick Haas <patrick.haas@gmx.net>
2023-11-10 21:45:30 +0100

    Implementierung der Keycloak-Persistenz für Benutzerkonten

--- metis-backend/metis-mitarbeiter/metis-mitarbeiter-adapters/src/main/java/io/metis/mitarbeiter/adapters/persistence/benutzerkonto/BenutzerkontoPersistenceAdapter.java
+++ metis-backend/metis-mitarbeiter/metis-mitarbeiter-adapters/src/main/java/io/metis/mitarbeiter/adapters/persistence/benutzerkonto/BenutzerkontoPersistenceAdapter.java
@@ -6,9 +6,7 @@ import io.metis.mitarbeiter.domain.benutzerkonto.Benutzerkonto;
 import io.metis.mitarbeiter.domain.benutzerkonto.BenutzerkontoId;
 import io.metis.mitarbeiter.domain.benutzerkonto.BenutzerkontoRepository;
 import io.metis.mitarbeiter.domain.gruppe.GruppeId;
-import org.springframework.stereotype.Repository;
 
-@Repository
 class BenutzerkontoPersistenceAdapter extends AbstractInMemoryPersistenceAdapter<Benutzerkonto, BenutzerkontoId> implements BenutzerkontoRepository {
     @Override
     public void assignGroupByEmployeeId(MitarbeiterId mitarbeiterId, GruppeId gruppeId) {
